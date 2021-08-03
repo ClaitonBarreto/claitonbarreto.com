1175878a5c89904fa7849fd8732ad0d53bf67259
claitonbarreto <claitonbarreto@gmail.com>
2021-08-03 11:50:07 -0300

    style(profile): add border in profile photo

--- src/components/ProfilePhoto/styles.ts
+++ src/components/ProfilePhoto/styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components'
 
+
 export const Photo = styled.img`
     width: 14%;
     border-radius: 100%;
     transition: all .2s;
+    opacity: .99;
+    border: 2px solid ${({theme}) => theme.primary};
     
     :hover {
         box-shadow: 1px 2px 2px #bbb;
